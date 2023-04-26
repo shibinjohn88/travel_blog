2c56f74e218dec8b4bf44eda10f36d59015a8ac7
Hansel <you@example.com>
2023-04-26 01:49:39 -0400

    added banner

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,9 +1,155 @@
-
 import React from 'react';
 import './home.css'
+import banner from '../../images/Travelog.jpg';
+import { Button, Card } from 'react-bootstrap';
+import island from '../../images/island.jpeg';
 
-function HelloWorld() {
-  return <h1 className="homeh1">Hello World!</h1>;
+
+
+
+function Home() {
+  return(
+    <div>
+   <img className= "travel_banner" src={banner} alt="Travelog Banner" />;
+
+   <h1>Welcome to Travelog</h1>
+
+   <div className='travel_cards'>
+   <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='card_button'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='card_button'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Img variant="top" src={island} />
+      <Card.Body>
+        <Card.Title>My Card</Card.Title>
+        <Card.Text>
+          your favorite island
+        </Card.Text>
+        <Button className='cardButton'>Blog</Button>
+      </Card.Body>
+    </Card>
+
+   </div>
+  </div>
+  );
 }
 
-export default HelloWorld;
+export default Home;
