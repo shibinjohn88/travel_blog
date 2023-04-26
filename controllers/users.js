e50d8982a04f4dc5d5ef51e335683633427618b3
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-04-26 19:26:08 -0400

    user model and controller created

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const users = express.Router()
+const User = require('../models/users.js')
+const bcrypt = require('bcrypt')
+
+//create a user using sign up details
+users.post('/', async (req, res) => {
+    let {password, ...remain} = req.body
+    const user = await User.create({
+        ...remain,
+        passwordDigest: await bcrypt.hash(password, 10)
+    })
+    res.json(user)
+})
+
+//read all user details
+users.get('/', async (req, res) => {
+    const users = await User.find()
+    res.json(users)
+})
+
+
+module.exports = users
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 require('dotenv').config()
 const port = process.env.PORT
 const blogController = require('./controllers/blogs.js')
+const userController = require('./controllers/users.js')
 const mongoose = require('mongoose')
 const bodyParser =require('body-parser')
 
@@ -11,12 +12,15 @@ const bodyParser =require('body-parser')
 //app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+
 app.get('/api', (req, res) => {
   res.send('API for Travel Blog web application')
 })
 
-//Redirecting all requests for blog to blog controller
+//redirecting to controllers
 app.use ('/api/blogs', blogController)
+app.use ('/api/users', userController)
 
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
