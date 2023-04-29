91edba751d05442f067a97572470828651b52170
Hansel <you@example.com>
2023-04-29 12:31:58 -0400

    home fetch api /search fetch api

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import './home.css'
 import banner from '../../images/travelogobanner.mp4';
 import { Button, Card } from 'react-bootstrap';
-import island from '../../images/island.jpeg';
 import travel_blog from '../../images/Travel_Blog.png';
 import Footer from '../footer/footer';
 const imageURL = 'http://localhost:3001/posters'
 
 
 
+
 function Home() {
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    fetch(imageURL)
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  
+
   return(
     <div className='home_body'>
    <video autoPlay loop className='travel_banner'>
@@ -21,161 +33,20 @@ function Home() {
 <h1>Top Places to Visit</h1>
    <div className='travel_cards'>
    
-   <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
+ {posts.map(post => (
+
+    <Card className='card' key={post.id}>
+     <Card.Img variant="top" src={post.image} />
+    <Card.Body>
+    <Card.Title>{post.title}</Card.Title>
+    <Card.Title>{post.place}</Card.Title>
+  <Card.Title>{post.country}</Card.Title>
+     <Card.Text>{post.highlights}</Card.Text>
+    <Button className='card_button'>Blog</Button>
+    </Card.Body>
     </Card>
 
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
-
-    <Card className='card'>
-      <Card.Img variant="top" src={island} />
-      <Card.Body>
-        <Card.Title>My Card</Card.Title>
-        <Card.Title>Place</Card.Title>
-        <Card.Title>Country</Card.Title>
-        <Card.Text>
-          Highlights
-        </Card.Text>
-        <Button className='card_button'>Blog</Button>
-      </Card.Body>
-    </Card>
+  ))}
     </div>
 
    </div>
